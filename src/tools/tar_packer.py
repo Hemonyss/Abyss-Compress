2678a0ca9892c6a0c5f344ffe20ec34bfa456d02
Hemonyss <hemonvoid@gmail.com>
2025-12-05 22:19:03 +0500

    fix: validate file_path type for proper processing

--- src/tools/tar_packer.py
+++ src/tools/tar_packer.py
@@ -3,8 +3,12 @@ from io import BytesIO
 from numpy import frombuffer, uint8
 import tarfile
 
-def create_tar(file_paths: list):
+def create_tar(file_paths: list | tuple | set | str) -> bytes: 
+    """str only if there is one path"""
     buffer = BytesIO()
+
+    if isinstance(file_paths, str):
+        file_paths = [file_paths]
 
     with tarfile.open(fileobj=buffer, mode='w') as tar:
         for file in file_paths:
@@ -16,7 +20,7 @@ def create_tar(file_paths: list):
 
     return tar_bytes
 
-def read_tar(tar_bytes: bytes):
+def read_tar(tar_bytes: bytes) -> dict:
     buffer = BytesIO(tar_bytes)
     file_dict = dict()
 
