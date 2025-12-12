eda949fa8e8918ecfc3cc3393e0f675d0f0c218a
Hemonyss <hemonvoid@gmail.com>
2025-12-12 22:43:42 +0500

    perf: change mmap to write for efficient batches write

--- src/algoritm/include/files_io.h
+++ src/algoritm/include/files_io.h
@@ -8,9 +8,8 @@ extern "C" {
     #endif
 
     void* read_file(const char* filename, size_t* size);
-    void* write_file(const char* filename, size_t size);
+    void* write_file(const char* filename, const void* data, size_t size);
     void mmap_free(void* data, size_t size);
-    void mmap_sync_free(void* data, size_t size);
 
     #ifdef __cplusplus
 }
--- src/algoritm/src/files_io.c
+++ src/algoritm/src/files_io.c
@@ -28,28 +28,16 @@ void* read_file(const char *filename, size_t* size) {
     return data == MAP_FAILED ? NULL : data;
 }
 
-void* write_file(const char* filename, size_t size) {
+void* write_file(const char* filename, const void* data, size_t size) {
     int fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0644);
     if (fd == -1) return NULL;
 
-    if (ftruncate(fd, size) == -1) {
-        close(fd);
-        return NULL;
-    }
-
-    void* data = mmap(NULL, size, PROT_READ | PROT_WRITE , MAP_SHARED, fd, 0);
+    ssize_t written = write(fd, data, size);
     close(fd);
 
-    return data == MAP_FAILED ? NULL : data;
+    return (written == (ssize_t)size) ? 0 : -1;
 }
 
 void mmap_free(void* data, size_t size) {
     if (data) munmap(data, size);
-}
-
-void mmap_sync_free(void* data, size_t size) {
-    if (data) {
-        msync(data, size, MS_SYNC);
-        munmap(data, size);
-    }
 }
